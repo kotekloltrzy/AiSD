fab1395317231b1f56b7339a4f9fe167ab2ce279
Michał Broda <147035736+kotekloltrzy@users.noreply.github.com>
2024-10-16 09:31:27 +0200

    Add files via upload

---
+++ 16.10.2024/Class2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _16._10._2024
+{
+    internal class Lista
+    {
+        public Node head;
+        public Node tail;
+        public int count;
+
+        public void AddFirst(int data)
+        {
+
+        }
+
+        public void AddLast(int data)
+        {
+
+        }
+
+        public void RemoveFirst()
+        {
+
+        }
+
+        public void RemoveLast()
+        {
+
+        }
+
+        public void Get(int n)
+        {
+
+        }
+    }
+}
