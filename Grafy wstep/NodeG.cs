711a19e99aab82e01c157d12c16d47a1eb310401
Michał Broda <147035736+kotekloltrzy@users.noreply.github.com>
2024-11-20 09:22:17 +0100

    Add files via upload

---
+++ Grafy wstep/NodeG.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafy_wstep
+{
+    internal class NodeG
+    {
+        List<NodeG> sasiedzi = new List<NodeG>();
+        int data;
+
+        NodeG(int liczba)
+        {
+            this.data = liczba;
+        }
+
+        public override string ToString()
+        {
+            return this.data.ToString();
+        }
+
+        public void dodajSasiada(NodeG sasiad)
+        {
+            if(!this.sasiedzi.Contains(sasiad))
+            {
+                sasiedzi.Add(sasiad);
+            }
+        }
+
+        public void podrozWglab(NodeG start)
+        {
+            List<NodeG> odwiedzone = new List<NodeG>();
+            odwiedzone.Add(start);
+            for(int i=0;i< this.sasiedzi.Count; i++)
+            {
+                if (!odwiedzone.Contains(sasiedzi[i]))
+                {
+                    odwiedzone.Add(sasiedzi[i]);
+                }
+                podrozWglab(sasiedzi[i]);
+            }
+        }
+
+        public void podrozWszerz(NodeG start)
+        {
+            List<NodeG> odwiedzone = new List<NodeG>();
+            odwiedzone.Add(start);
+            for(int i=0; i<this.sasiedzi.Count; i++)
+            {
+
+            }
+        }
+
+        public void chodHowmana(NodeG start)
+        {
+            List<NodeG> odwiedzone = new List<NodeG>();
+                odwiedzone.Add(start);
+        }
+    }
+}
