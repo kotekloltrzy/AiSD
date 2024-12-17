e3f7083f1cbedcb9f50547209b0e58d6bcdcda62
Michał Broda <147035736+kotekloltrzy@users.noreply.github.com>
2024-12-17 23:29:10 +0100

    Add files via upload

---
+++ Kolokwium przygotowanie1/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Kolokwium_przygotowanie1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(77, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Merge Sort";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(73, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Insert Sort";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(89, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Counting Sort";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 88);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(75, 17);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Quick Sort";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 111);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(80, 17);
+            this.radioButton5.TabIndex = 5;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Bubble Sort";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Location = new System.Drawing.Point(35, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(100, 140);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(159, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 46);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sortuj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(306, 75);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(84, 20);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(303, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(283, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Wybierz rozmiar tabeli od 2 do 10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(303, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Tabela:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Kolokwium przygotowanie1/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace Kolokwium_przygotowanie1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] tabela = new int[10];
+        Random rnd = new Random();
+
+        private void GenerateArray(int[] tab, int i)
+        {
+            for(int j=0; j<i; j++)
+            {
+                tab[j] = rnd.Next(0,21);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = Decimal.ToInt32(numericUpDown1.Value);
+            int[] tablica2 = new int[i];
+            GenerateArray(tablica2, i);
+            label1.Text = ToText(tablica2);
+            label1.Visible = true;
+            tabela = tablica2;
+        }
+
+        private string ToText(int[] tabela)
+        {
+            string wynik = "";
+            for(int i =0; i < tabela.Length; i++)
+            {
+                wynik += tabela[i] + " ";
+            }
+            return wynik;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked == true)
+            {
+                MergeSort(tabela);
+                label1.Text = ToText(tabela);
+            }
+            else if (radioButton2.Checked == true)
+            {
+                InsertSort(tabela);
+                label1.Text = ToText(tabela);
+            }
+            else if (radioButton3.Checked == true)
+            {
+                label1.Text = ToText(CountingSort(tabela));
+            }
+            else if (radioButton4.Checked == true)
+            {
+                QuickSort(tabela, 0, tabela.Length - 1);
+                label1.Text = ToText(tabela);
+            }
+            else if (radioButton5.Checked == true)
+            {
+                BubbleSort(tabela);
+                label1.Text = ToText(tabela);
+            }
+        }
+
+        // Merge Sort
+
+        private void MergeSort(int[] tabela)
+        {
+            int length = tabela.Length;
+            if (length <= 1) return;
+            int middle = length/2;
+            int[] lTab = new int[middle];
+            int[] rTab = new int[length - middle];
+            int i = 0, j = 0;
+            for (; i < length; i++)
+            {
+                if (i < middle)
+                {
+                    lTab[i] = tabela[i];
+                }
+                else
+                {
+                    rTab[j++] = tabela[i];
+                }
+            }
+            MergeSort(lTab);
+            MergeSort(rTab);
+            Merge(lTab, rTab, tabela);
+        }
+
+        private void Merge(int[] lTab, int[] rTab, int[] tabela)
+        {
+            int lSize = tabela.Length/2;
+            int rSize = tabela.Length - lSize;
+            int i = 0, l = 0,r = 0;
+
+            while(l < lSize && r < rSize)
+            {
+                if (lTab[l] < rTab[r])
+                {
+                    tabela[i++] = lTab[l++];
+                }
+                else
+                {
+                    tabela[i++] = rTab[r++];
+                }
+            }
+            while (l < lSize)
+            {
+                tabela[i++] = lTab[l++];
+            } 
+            while (r < rSize)
+            {
+                tabela[i++] = rTab[r++];
+            }
+        }
+
+        // Quick Sort
+
+        private void QuickSort(int[] tabela, int start, int end)
+        {
+            if (start >= end) return;
+            int p = Divide(tabela, start, end);
+            QuickSort(tabela, start, p - 1);
+            QuickSort(tabela, p+1, end);
+        }
+
+        private int Divide(int[] tabela, int start, int end)
+        {
+            int p = tabela[end];
+            int i = start - 1;
+            for(int j=start; j < end; j++)
+            {
+                if (tabela[j] < p)
+                {
+                    i++;
+                    Swap(tabela, i, j);
+                }
+            }
+            i++;
+            Swap(tabela, i,end);
+            return i;
+        }
+
+        private void Swap(int[] tabela, int i, int j)
+        {
+            int temp = tabela[i];
+            tabela[i] = tabela[j];
+            tabela[j] = temp;
+        }
+
+        private int GetMaxVal(int[] tabela)
+        {
+            int max = tabela[0];
+            for(int i=1; i<tabela.Length; i++)
+            {
+                if(tabela[i] > max)
+                {
+                    max = tabela[i];
+                }
+            }
+            return max;
+        }
+
+        // Counting Sort
+
+        private int[] CountingSort(int[] tabela)
+        {
+            int max = GetMaxVal(tabela);
+            int[] count = new int[max+1];
+            for(int i=0; i < max; i++)
+            {
+                count[i] = 0;
+            }
+            for(int i=0; i < tabela.Length; i++)
+            {
+                count[tabela[i]]++;
+            }
+            for(int i=0,j=0; i <= max; i++)
+            {
+                while(count[i] > 0)
+                {
+                    tabela[j++] = i;
+                    count[i]--;
+                }
+            }
+            return tabela;
+        }
+
+        // Bubble Sort
+
+        private void BubbleSort(int[] tabela)
+        {
+            for(int i = 0; i < tabela.Length; i++)
+            {
+                for(int j=0; j < tabela.Length; j++)
+                {
+                    if (tabela[j] > tabela[i])
+                    {
+                        Swap(tabela, i,j);
+                    }
+                }
+            }
+        }
+
+        // Insert Sort
+
+        private void InsertSort(int[] tabela)
+        {
+            for(int i=1; i < tabela.Length; i++)
+            {
+                int temp = tabela[i];
+                int j = i - 1;
+                while(j>= 0 && temp < tabela[j])
+                {
+                    tabela[j+1] = tabela[j];
+                    j--;
+                }
+                tabela[j + 1] = temp;
+            }
+        }
+    }
+}
